ca80267a7769503a197e8b945cc7216c240789b8
Danny Kopping <dannykopping@gmail.com>
2012-12-28 17:12:46 +0200

    Cleaned up story summaries parsing

--- scrape/SnopesScraper.php
+++ scrape/SnopesScraper.php
@@ -1,6 +1,4 @@
 <?php
-use Symfony\Component\DomCrawler\Crawler;
-
 /**
  *  This class scrapes the Snopes site for various resources
  */
@@ -48,11 +46,14 @@ class SnopesScraper
 
         for ($x = 0; $x < $catCount; $x++) {
             $title = htmlqp($homepageHTML, "td[align='LEFT'][valign='CENTER']:not(img)")->eq($x)->text();
-            if(empty($title))
+            if (empty($title)) {
                 continue;
+            }
 
             $url = htmlqp($homepageHTML, "td[align='LEFT'][valign='CENTER']:not(img)")->eq($x)->find("a")->attr("href");
-            $icon = htmlqp($homepageHTML, "td[align='LEFT'][valign='CENTER']:not(font)")->eq($x)->find("img")->attr("src");
+            $icon = htmlqp($homepageHTML, "td[align='LEFT'][valign='CENTER']:not(font)")->eq($x)->find("img")->attr(
+                "src"
+            );
 
             $categories[$title] = array(
                 "title" => $title,
@@ -60,6 +61,9 @@ class SnopesScraper
                 "icon" => $icon,
                 "subcategories" => self::getSubcategories($url)
             );
+
+            if(empty($categories[$title]["subcategories"]))
+                $categories[$title]["stories"] = self::getStorySummaries($url);
         }
 
         return $categories;
@@ -87,8 +91,9 @@ class SnopesScraper
         for ($x = 0; $x < $subcatCount; $x++) {
             try {
                 $title = self::clean(htmlqp($categoryHTML, "td[height='80']")->eq($x)->find("a")->text());
-                if(empty($title))
+                if (empty($title)) {
                     continue;
+                }
 
                 // get the current element's previous sibling, which should contain an icon
                 $icon = htmlqp($categoryHTML, "td[height='80']")->eq($x)->prev()->find("img")->attr("src");
@@ -113,7 +118,8 @@ class SnopesScraper
                 "title" => $title,
                 "url" => $url,
                 "icon" => $icon,
-                "description" => $description
+                "description" => $description,
+                "stories" => self::getStorySummaries($url)
             );
         }
 
@@ -145,82 +151,136 @@ class SnopesScraper
 
         $q = new DOMXPath($dom);
 
-        $story = htmlqp($categoryHTML, "table[width='90%'][align='CENTER']")->children("td")->last()->html();
-        $story = str_replace("&#13;", "", $story);
-        $story = str_replace("\r\n", "", $story);
-        $story = str_replace("\n", "", $story);
-        $story = str_replace("<br /><br />", "<br/><br/>", $story);
-        $story = str_replace("<br/><br/>", "\n\n-----------------\n\n", $story);
+        $storiesHTML = htmlqp($categoryHTML, "table[width='90%'][align='CENTER']")->children("td")->last()->html();
+        $storiesHTML = str_replace("&#13;", "", $storiesHTML);
+        $storiesHTML = str_replace("\r\n", "", $storiesHTML);
+        $storiesHTML = str_replace("\n", "", $storiesHTML);
+        $storiesHTML = str_replace("<br /><br />", "<br/><br/>", $storiesHTML);
+        $storiesHTML = str_replace("<br/><br/>", "\n\n-----------------\n\n", $storiesHTML);
 
-        // try to parse list for pages like:
-        // http://www.snopes.com/cokelore/cokelore.asp
-        $result = self::parseNewSummaryPage($story);
-
-        // otherwise, try to parse list for pages like:
-        // http://www.snopes.com/military/military.asp
-        if (!$result) {
-            $result = self::parseOldSummaryPage($story);
-        }
-
-
-        return $result;
+        return self::parseStoriesPage($storiesHTML);
     }
 
-    private static function findBestMatch(DOMDocument $dom, DOMXPath $q, array $xpaths, $url)
+    /**
+     * Parse stories summary page
+     *
+     * @param $storiesHTML
+     * @return array|null
+     */
+    private static function parseStoriesPage($storiesHTML)
     {
-        $bestMatch = null;
-        $longestMatch = null;
-        $matchLength = 0;
+        // try parsing "newer" page format, otherwise fall back to "older" page parsing
+        $parsed = self::parseNewSummaryPage($storiesHTML);
+        if(!$parsed)
+            return self::parseOldSummaryPage($storiesHTML);
 
-        foreach ($xpaths as $xpath) {
-            $match = $q->query($xpath);
-            if (!$match || $match->length <= 0) {
+        return $parsed;
+    }
+
+    /**
+     * Parse "newer" page format like:
+     * http://www.snopes.com/cokelore/cokelore.asp
+     *
+     * @param $storiesHTML
+     * @return array|null
+     */
+    private static function parseNewSummaryPage($storiesHTML)
+    {
+        preg_match_all(
+            '%(<a name="\w+"></a>)?<img src="(.+)" width="?36"? height="?36"? alt=".+" title=".+" align="?absmiddle"? /><font.+\+1"?>.+</font.+<a href="/?(.+)" onmouseover="window.status=\'.+\';return.+>(.+)</a>.+<br />(.+)$%im',
+            $storiesHTML,
+            $result,
+            PREG_PATTERN_ORDER
+        );
+
+        if (count($result) == 0 || count($result[0]) == 0) {
+            return null;
+        }
+
+        $summaries = array();
+        $counter = 0;
+        for ($i = 0; $i < count($result[0]); $i++) {
+            $rating = self::getClassificationByImg($result[2][$counter]);
+            $url = self::clean($result[3][$counter]);
+            $title = ucfirst(self::clean($result[4][$counter]));
+
+            $summary = $result[5][$counter];
+            $summary = preg_replace('%<font.+</font>%im', '', $summary);
+            $summary = ucfirst(self::clean(preg_replace('%(</?[^<]+>)%im', '', $summary)));
+
+            $summaries[] = array(
+                "rating" => $rating,
+                "summary" => $summary,
+                "title" => $title,
+                "url" => $url
+            );
+
+            $counter++;
+        }
+
+        return $summaries;
+    }
+
+    /**
+     * Parse "older" page format like:
+     * http://www.snopes.com/military/military.asp
+     *
+     * @param $storiesHTML
+     * @return array
+     */
+    private static function parseOldSummaryPage($storiesHTML)
+    {
+        preg_match_all(
+            '%(<a name="\w+"></a>)?<img.+?src="(.+)".+?align="ABSMIDDLE"/>(.+)%im',
+            $storiesHTML,
+            $result,
+            PREG_PATTERN_ORDER
+        );
+
+        $summaries = array();
+        $counter = 0;
+        for ($i = 0; $i < count($result[0]); $i++) {
+
+            $title = ucfirst(
+                self::clean(
+                    preg_replace(
+                        '%(.+)?<a href="([^"]+)"(?:.+status=\'([^\']+).+)?>(.+)</a>(.+)?%im',
+                        '$3',
+                        $result[3][$counter]
+                    )
+                )
+            );
+
+            $summary = self::clean(
+                preg_replace(
+                    '%(.+)?<a href="([^"]+)"(?:.+status=\'([^\']+).+)?>(.+)</a>(.+)?%im',
+                    '$1$4$5',
+                    $result[3][$counter]
+                )
+            );
+            $summary = preg_replace('%<font.+</font>%im', '', $summary);
+            $summary = preg_replace('%(</?[^<]+>)%im', '', $summary);
+            $summary = ucfirst(self::clean($summary));
+
+            $url = self::clean(
+                preg_replace('%(.+)?<a href="([^"]+).+>(.+)</a>(.+)?%im', '$2', $result[3][$counter])
+            );
+
+            if ($summary == '= unclassifiable veracity' || $title == '= unclassifiable veracity' || $url == '= unclassifiable veracity') {
                 continue;
             }
 
-            $raw = $dom->saveHTML($match->item(0));
-
-            $icons = array(
-                self::TRUTH_ICON,
-                self::FALSE_ICON,
-                self::MULTI_TRUTH_ICON,
-                self::MIXED_ICON,
-                self::PARTLY_TRUE_ICON,
-                self::UNDETERMINED_ICON,
-                self::UNCLASSIFIABLE_ICON,
-                self::LEGEND_ICON
+            $summaries[] = array(
+                "rating" => self::getClassificationByImg($result[2][$counter]),
+                "summary" => $summary,
+                "title" => $title,
+                "url" => $url
             );
 
-            $hasOneIcon = false;
-            foreach ($icons as $icon) {
-                if (strpos($raw, $icon) !== false) {
-                    $hasOneIcon = true;
-                    break;
-                }
-            }
-
-            // check by non-existence of "rating key" string and existence of truth indicator icon(s)
-            // - if no matches can be found with icons (which are more likely to be valid)), select the longest result
-            if (strpos(strtolower($raw), "ratings key") === false) {
-                if ($hasOneIcon) {
-                    $bestMatch = $match;
-                } elseif (!$hasOneIcon) {
-                    $longestMatch = $match;
-                    $matchLength = strlen($raw);
-                }
-            }
+            $counter++;
         }
 
-        $domH = null;
-        if (!$bestMatch) {
-//            $domH = $dom->saveHTML();
-
-            if ($longestMatch) {
-                $bestMatch = $longestMatch;
-            }
-        }
-
-        return $bestMatch;
+        return $summaries;
     }
 
     private static function getClassificationByImg($imgURL)
@@ -254,108 +314,6 @@ class SnopesScraper
         }
 
         return null;
-    }
-
-    private static function getPageType($pageHTML)
-    {
-        $pageHTML = strtolower($pageHTML);
-        if (strpos($pageHTML, '<b>ratings key</b>') !== false) {
-            return self::STORIES_PAGE;
-        }
-
-        return self::CATEGORY_PAGE;
-    }
-
-    private static function parseNewSummaryPage($story)
-    {
-        preg_match_all(
-            '%(<a name="\w+"></a>)?<img src="(.+)" width="?36"? height="?36"? alt=".+" title=".+" align="?absmiddle"? /><font.+\+1"?>.+</font.+<a href="/?(.+)" onmouseover="window.status=\'.+\';return.+>(.+)</a>.+<br />(.+)$%im',
-            $story,
-            $result,
-            PREG_PATTERN_ORDER
-        );
-
-        if (count($result) == 0 || count($result[0]) == 0) {
-            return null;
-        }
-
-        $objs = array();
-        $counter = 0;
-        for ($i = 0; $i < count($result[0]); $i++) {
-            $rating = self::getClassificationByImg($result[2][$counter]);
-            $url = self::clean($result[3][$counter]);
-            $title = ucfirst(self::clean($result[4][$counter]));
-
-            $summary = $result[5][$counter];
-            $summary = preg_replace('%<font.+</font>%im', '', $summary);
-            $summary = ucfirst(self::clean(preg_replace('%(</?[^<]+>)%im', '', $summary)));
-
-            $objs[] = array(
-                "rating" => $rating,
-                "summary" => $summary,
-                "title" => $title,
-                "url" => $url
-            );
-
-            $counter++;
-        }
-
-        return $objs;
-    }
-
-    private static function parseOldSummaryPage($story)
-    {
-        preg_match_all(
-            '%(<a name="\w+"></a>)?<img.+?src="(.+)".+?align="ABSMIDDLE"/>(.+)%im',
-            $story,
-            $result,
-            PREG_PATTERN_ORDER
-        );
-
-        $objs = array();
-        $counter = 0;
-        for ($i = 0; $i < count($result[0]); $i++) {
-
-            $title = ucfirst(
-                self::clean(
-                    preg_replace(
-                        '%(.+)?<a href="([^"]+)"(?:.+status=\'([^\']+).+)?>(.+)</a>(.+)?%im',
-                        '$3',
-                        $result[3][$counter]
-                    )
-                )
-            );
-
-            $summary = self::clean(
-                preg_replace(
-                    '%(.+)?<a href="([^"]+)"(?:.+status=\'([^\']+).+)?>(.+)</a>(.+)?%im',
-                    '$1$4$5',
-                    $result[3][$counter]
-                )
-            );
-            $summary = preg_replace('%<font.+</font>%im', '', $summary);
-            $summary = preg_replace('%(</?[^<]+>)%im', '', $summary);
-            $summary = ucfirst(self::clean($summary));
-
-            $url = self::clean(
-                preg_replace('%(.+)?<a href="([^"]+).+>(.+)</a>(.+)?%im', '$2', $result[3][$counter])
-            );
-
-            if ($summary == '= unclassifiable veracity' || $title == '= unclassifiable veracity' || $url == '= unclassifiable veracity') {
-                continue;
-            }
-
-            $objs[] = array(
-                "rating" => self::getClassificationByImg($result[2][$counter]),
-                "summary" => $summary,
-                "title" => $title,
-                "url" => $url
-            );
-
-            $counter++;
-        }
-
-        return $objs;
     }
 
     private static function clean($str)
